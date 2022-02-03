1a6ab9343e5dab1f82c123194342d68b8b82b4df
Vonckad <Vonkad@yandex.ru>
2022-02-03 14:52:56 +0300

    Initial Commit

---
+++ WFApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WFApp
+//
+//  Created by Vlad Ralovich on 3.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
